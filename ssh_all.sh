de7a0cbdd0ce48e6e2b110599c7bea10625b2c79
Chris Bergeron <cbergeron@gmail.com>
2017-01-26 20:11:12 +0000

    Added package mlocate.

---
+++ ssh_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then 
+	echo -e "\nUsage:\n$0 hostname(s) command\n";
+	echo -en "Ansible hosts:\n"
+	for i in `cat hosts | grep -v '^\[' | grep -v "^#" | cut -f2 -d":" | cut -f1 -d" "`; do echo -n "$i "; done
+	echo
+	echo
+fi
+
+for i in `cat hosts | grep -v '^\[' | grep -v "^#" | cut -f2 -d":" | cut -f1 -d" "`; do echo "$i "; ssh $i "$1"; done
+
+
+#for i in {1..5}; do echo "Pizero$i:"; ssh pizero$i "sudo netstat -tpl | grep -v Local | grep -v Active | grep -v '::' | grep -v smtp | grep -v ssh"; done
